1a8f918737a2c4a651c37f2b117917624dccbba9
BuildTools <unconfigured@null.spigotmc.org>
2021-01-17 23:51:17 +0100

    Add save and load in the cookie

--- task/script.js
+++ task/script.js
@@ -1,13 +1,270 @@
-openAddTask();
-isonchange();
+closeAddTask();
+onChange_addtask_option();
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+/*var mess = "";
+
+var ca = getTask()
+for(var i = 0; i < ca.length; i++) {
+	var c = ca[i];
+	mess += '<label class="box draggable" draggable="true"><input type="checkbox" id="action_1" name="scales">' + c.substring(name.length, c.length) + '</label>';
+}
+document.getElementById("testtt59").innerHTML = mess;*/
+
+
+
+
+
+
+
+var lists = [];
+
+var list1 = [];
+var list2 = [];
+var list3 = [];
+
+list1.push("hello1");
+list1.push("hello2");
+list2.push("hello3");
+list2.push("hello4");
+list3.push("hello5");
+list3.push("hello6");
+
+
+lists.push(list1);
+lists.push(list2);
+lists.push(list3);
+
+//setCookie("save_list1_quest", lists, 365);
+//setCookie("task2", "zzz", 365);
+
+
+
+
+class Task {
+	constructor(name, desc) {
+		this.name = name;
+		this.desc = desc;
+		this.check = false;
+	}
+
+	/*constructor(parse) {
+		let m = JSON.parse(parse);
+		this.name = m.name;
+		this.desc = m.desc;
+		this.check = m.check;
+	}*/
+
+	getName() {
+		return this.name;
+	}
+
+	getDesc() {
+		return this.desc;
+	}
+
+	isCheck() {
+		return this.check;
+	}
+}
+
+class List {
+	constructor(name, task) {
+		this.name = name;
+		this.task = task;
+	}
+
+	getName() {
+		return this.name;
+	}
+
+	getTasks() {
+		return this.task;
+	}
+}
+
+load();
 
 const draggables = document.querySelectorAll('.draggable');
 const containers = document.querySelectorAll('.containertask');
+
+let myTask1 = new Task("quest Journaliere", "est 4 quete qui ce fait tout les jours");
+let myTask2 = new Task("10 minerais", "il faut récupéer 10 minerais et ce fait tout les jours");
+
+var listt = [];
+
+listt.push(myTask1);
+listt.push(myTask2);
+
+let myTask6 = new List("Genchin impact", listt);
+
+
+
+
+
+
+let myTask21 = new Task("2quest Journaliere", "2est 4 quete qui ce fait tout les jours");
+let myTask22 = new Task("210 minerais", "2il faut récupéer 10 minerais et ce fait tout les jours");
+
+var listt2 = [];
+
+listt2.push(myTask21);
+listt2.push(myTask22);
+
+let myTask26 = new List("Genchin impact2", listt2);
+
+
+var ssave = [];
+
+ssave.push(myTask6);
+ssave.push(myTask26);
+
+
+setCookie("save", JSON.stringify(ssave), 365);
+
+
+//var jjj =  Object.assign(new Task, JSON.parse(getCookie("save"))[1]);
+
+//alert(jjj.getName());
+
+
+
+
+
+function load() {
+	let list = getList();
+
+	let html = "";
+
+
+	for(let i = 0; i < list.length; i++) {
+		let list2 = list[i];
+		html += '<div class="container containertask">';
+			html += '<h1>' + list2.getName() + '</h1>';
+			for(let i2 = 0; i2 < list2.getTasks().length; i2++) {
+				let task = list2.getTasks()[i2];
+				html += '<label class="box draggable" draggable="true"><input type="checkbox" id="action_1" name="scales">' + task.getName() + '</label>';
+			}
+			html += '<p class="box morebox" onclick="openAddTask()">+</p>';
+		html += '</div>';
+	}
+
+	html += '<div class="container morecontainer"></div>';
+
+	document.getElementById("contt2").innerHTML = html;
+}
+
+
+
+
+
+
+
+
+//alert(getCookie("save"));
+
+
+//alert(getList()[1].getTasks()[0].getName());
+//alert(getList()[1].getName());
+
+function getList() {
+	var lists = [];
+	let save_list_list_task = JSON.parse(getCookie("save"));
+	
+	for(let i = 0; i < save_list_list_task.length; i++) {
+		let save_list_task = save_list_list_task[i];
+		//alert("save_list_task " + save_list_list_task[i].task.length);
+		let list_task = [];
+		for(let i2 = 0; i2 < save_list_task.task.length; i2++) {
+			let test2 = Object.assign(new Task, save_list_task.task[i2])
+			list_task.push(test2);
+			//alert(test2.getName() + " " + test2.getDesc() + " " + test2.isCheck());
+		}
+		lists.push(new List(save_list_task.name, list_task));
+	}
+	return lists;
+}
+
+
+
+// Cookie
+function setCookie(cname, cvalue, exdays) {
+	var date = new Date();
+	date.setTime(date.getTime() + (exdays*24*60*60*1000));
+	var expires = "expires="+ date.toUTCString();
+	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+  
+function getCookie(cname) {
+	var name = cname + "=";
+	var decodedCookie = decodeURIComponent(document.cookie);
+	var ca = decodedCookie.split(';');
+	for(var i = 0; i <ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
+
+function checkCookie() {
+	var username = getCookie("username");
+	if (username != "") {
+		alert("Welcome again " + username);
+	} else {
+	username = prompt("Please enter your name:", "");
+		if (username != "" && username != null) {
+			setCookie("username", username, 365);
+		}
+	}
+}
+
+
+
+/*function getList() {
+	var lists = [];
+	var name = "save_";
+	var ca = document.cookie.split(';');
+	for(var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			lists.push(c.substring(name.length, c.length));
+		}
+	}
+	return lists;
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
